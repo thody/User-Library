abe3fa99e889cacddc2baa483ba9b0ddcbd53661
Adam Thody <thody@mac.com>
2009-08-16 22:00:07 -0400

    Added config, organized folders

---
+++ config/user.php
@@ -0,0 +1,5 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+$config['user_table'] = 'users';
+$config['identity_column'] = 'email';
+$config['adapter'] = 'User_Db';
